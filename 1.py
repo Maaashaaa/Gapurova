5931045ee4b49958d76da9855f503ccf41174044
Maaashaaa <161114691+Maaashaaa@users.noreply.github.com>
2024-02-24 14:03:34 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,3 +1,11 @@
-room=int(input("1"))
-#floor
-floor = room//8
+print("Введите номер квартиры")
+room = int(input())
+
+stage =  int(room//8+1)
+floor = int(room-(stage-1)*8)
+
+
+if (room % 8) == 0:
+ print(stage-1, floor+8)
+else:
+ print(stage, floor)
---
+++ 2.py
@@ -0,0 +1,4 @@
+num1 = float(input("Введите первое число:"))
+num2 = float(input("Введите второе число:"))
+sum_numbers = num1 + num2
+print("Сумма чисел равна:", sum_numbers)
